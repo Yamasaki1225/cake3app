d798d0b252f04830d558faf97bce64e967fecc2f
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2018-12-07 18:31:40 +0900

    フォワードとリダイレクト

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,10 +5,14 @@ class HelloController extends AppController {
     public $name = 'Hello';
     public $autoRender = false;
 
-    public function index() {
-        $this->name = 'Hello';
-        $this->autoRender = false;
-        echo "Hello world!";
+    public function index(){
+        echo "aa";
+        // $this->setAction("other"); //フォワード
+        $this->redirect("./other"); //リダイレクト
+    }
+
+    public function other(){
+        echo "これは、index以外の表示です。";
     }
 }
 
