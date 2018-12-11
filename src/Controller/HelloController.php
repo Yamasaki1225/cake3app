b39c5d019c4cb9920092da22fe82383a15f20374
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2018-12-11 18:28:18 +0900

    ビューテンプレートとレイアウト

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,14 +2,15 @@
 namespace App\Controller;
 
 class HelloController extends AppController {
-    public $name = "hello";
-    public $autoRender = true;
+    public function initialize(){
+    $this->name = "hello";
+    $this->autoRender = false;
+    $this->viewBuilder()->autoLayout(false);
+    }
 
     public function index(){
-        //src/Template/Helloファルダの中にある、テンプレートを読みこんでページを表示させる
+        $this->viewBuilder()->autoLayout(true);
         $this->autoRender = true;
-        //レイアウト機能を使った自動レイアウトをOFFにする
-        $this->viewBuilder()->autoLayout(false);
     }
     public function other(){
         $this->viewBuilder()->autoLayout(false);
