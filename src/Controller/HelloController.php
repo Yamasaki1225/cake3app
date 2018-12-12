d12bf1b96a79694e3765fa8764264956124875e1
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2018-12-12 17:14:00 +0900

    layoutの使用

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,17 +4,18 @@ namespace App\Controller;
 class HelloController extends AppController {
     public function initialize(){
     $this->name = "hello";
-    $this->autoRender = false;
-    $this->viewBuilder()->autoLayout(false);
+    // $this->autoRender = false;
+    $this->viewBuilder()->autoLayout(true);
+    $this->viewBuilder()->layout('Hello');
     }
 
     public function index(){
-        $this->viewBuilder()->autoLayout(true);
-        $this->autoRender = true;
+        // $this->viewBuilder()->autoLayout(true);
+        // $this->autoRender = true;
     }
     public function other(){
-        $this->viewBuilder()->autoLayout(false);
-        $this->autoRender = true;
+        // $this->viewBuilder()->autoLayout(false);
+        // $this->autoRender = true;
     }
 }
 ?>
