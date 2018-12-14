bac2dfaf8abc3bc8df4b33f8cf5fa09f1ae6a189
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2018-12-14 13:04:53 +0900

    送信フォームと送信結果ページ作成

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,22 +3,22 @@ namespace App\Controller;
 
 class HelloController extends AppController {
     public function initialize(){
-    $this->name = "hello";
-    // $this->autoRender = false;
-    $this->viewBuilder()->autoLayout(true);
-    $this->viewBuilder()->layout('Hello');
+        $this->viewBuilder()->layout('Hello');
+        $this->set('msg', 'Hello/index');
+        $this->set('footer', 'Hello/footer2');
     }
 
     public function index(){
-        $this->set('msg', 'ヘッダーエレメント!!');
-        $n = rand(1,2);
-        $this->set('footer', 'Hello/footer' . $n);
-        // $this->viewBuilder()->autoLayout(true);
-        // $this->autoRender = true;
     }
-    public function other(){
-        // $this->viewBuilder()->autoLayout(false);
-        // $this->autoRender = true;
+    public function sendForm(){
+        $str = $this->request->query['text1'];
+        $result = "";
+        if ($str != ""){
+            $result = "you type:". $str;
+        } else {
+            $result = "empty.";
+        }
+        $this->set("result", $result);
     }
 }
 ?>
